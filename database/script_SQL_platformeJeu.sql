b2aa671493bdbaf4df6abb4303ef9835c95b5637
py01scc <gatien.clerc@eduvaud.ch>
2026-02-26 11:48:58 +0100

    feat: script db and data

---
+++ database/script_SQL_platformeJeu.sql
@@ -0,0 +1,100 @@
+/***********************************************************************************************************************
+ * Program name :           script_SQL_platformeLAN.sql
+ * Description :            Application principale
+ * Author :                 Gatien clerc
+ * Creation date :          26.02.2026
+ * Modified by :            -
+ * Modification date :      26.02.2026
+ * Version :                0.1
+ **********************************************************************************************************************/
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema plaformeLAN
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `plaformeLAN` ;
+
+-- -----------------------------------------------------
+-- Schema plaformeLAN
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `plaformeLAN` DEFAULT CHARACTER SET utf8 ;
+USE `plaformeLAN` ;
+
+-- -----------------------------------------------------
+-- Table `plaformeLAN`.`Roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `plaformeLAN`.`Roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `plaformeLAN`.`Users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `plaformeLAN`.`Users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `lastname` VARCHAR(25) NOT NULL,
+  `firstname` VARCHAR(25) NOT NULL,
+  `pseudo` VARCHAR(25) NOT NULL,
+  `birthdate` DATE NOT NULL,
+  `passeword` VARCHAR(65) NOT NULL,
+  `email` VARCHAR(255) CHARACTER SET 'cp1251' NOT NULL,
+  `role_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `pseudo_UNIQUE` (`pseudo` ASC) INVISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  INDEX `fk_Users_Roles_idx` (`role_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Users_Roles`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `plaformeLAN`.`Roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `plaformeLAN`.`LANs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `plaformeLAN`.`LANs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  `date` DATE NOT NULL,
+  `location` VARCHAR(50) NOT NULL,
+  `max_participants` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `plaformeLAN`.`Users_take_part_in_LANs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `plaformeLAN`.`Users_take_part_in_LANs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `LAN_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Users_take_part_in_LANs_Users1_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_Users_take_part_in_LANs_LANs1_idx` (`LAN_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Users_take_part_in_LANs_Users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `plaformeLAN`.`Users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Users_take_part_in_LANs_LANs1`
+    FOREIGN KEY (`LAN_id`)
+    REFERENCES `plaformeLAN`.`LANs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
